daef9b72336d67c4d03dc09aeebe981c22af587d
Christian Curbelo <chris94curbelo@gmail.com>
2025-05-18 12:31:10 -0300

    subiendo arreglos de archer

--- src/Library/Characters/Archer.cs
+++ src/Library/Characters/Archer.cs
@@ -1,8 +1,11 @@
+using Library;
+
 namespace Ucu.Poo.RoleplayGame;
 
-public class Archer
+public class Archer : IPersonaje
 {
     private int health = 100;
+    private int initialHealth = 100;
 
     public Archer(string name)
     {
@@ -10,51 +13,81 @@ public class Archer
     }
 
     public string Name { get; set; }
-    
-    public Bow Bow { get; set; }
-
-    public Helmet Helmet { get; set; }
-
-    public Shield Shield { get; set; }
-
-    public int AttackValue
-    {
-        get
-        {
-            return Bow.AttackValue;
-        }
-    }
-
-    public int DefenseValue
-    {
-        get
-        {
-            return Helmet.DefenseValue;
-        }
-    }
 
     public int Health
     {
-        get
-        {
-            return this.health;
-        }
-        private set
-        {
-            this.health = value < 0 ? 0 : value;
-        }
+        get => this.health;
+        set => this.health = value < 0 ? 0 : value;
     }
 
-    public void ReceiveAttack(int power)
+    public int InitialHealth
     {
-        if (this.DefenseValue < power)
+        get => this.initialHealth;
+        set => this.initialHealth = value;
+    }
+
+    public Bow Bow { get; set; }
+
+    public Shield Shield { get; set; }
+
+    public Helmet Helmet { get; set; }
+
+    public Axe Axe { get; set; } = null;
+
+    public Staff Staff { get; set; } = null;
+
+    public SpellsBook SpellsBook { get; set; } = null;
+
+    public int AttackPoints { get; set; } = 0;
+
+    public int AttackValue
+    {
+        get => Bow != null ? Bow.AttackValue : 0;
+        set => AttackPoints = value;
+    }
+
+    public int GetTotalArmor()
+    {
+        int shieldValue = Shield != null ? Shield.DefenseValue : 0;
+        int helmetValue = Helmet != null ? Helmet.DefenseValue : 0;
+        return shieldValue + helmetValue;
+    }
+
+    public int GetTotalAtack()
+    {
+        return Bow != null ? Bow.AttackValue : 0;
+    }
+
+    public string HealPj()
+    {
+        this.Cure();
+        return $"{Name} fue curado.";
+    }
+
+    public void ShowElements()
+    {
+        Console.WriteLine($"{Name} tiene:");
+        if (Bow != null) Console.WriteLine($"- Arco: {Bow.AttackValue} ataque");
+        if (Helmet != null) Console.WriteLine($"- Casco: {Helmet.DefenseValue} defensa");
+        if (Shield != null) Console.WriteLine($"- Escudo: {Shield.DefenseValue} defensa");
+    }
+
+    public void Attack(IPersonaje personaje)
+    {
+        personaje.ReceiveAttack(this.AttackValue);
+    }
+
+    public void ReceiveAttack(int damage)
+    {
+        int defensa = GetTotalArmor();
+        if (defensa < damage)
         {
-            this.Health -= power - this.DefenseValue;
+            this.Health -= damage - defensa;
         }
     }
 
     public void Cure()
     {
-        this.Health = 100;
+        this.Health = this.InitialHealth;
     }
 }
